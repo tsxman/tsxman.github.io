2ea94d0ec479568133452ce5a4ec68cf99dfda20
Vasily <etovasily@gmail.com>
2026-07-18 08:05:14 +0300

    build fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { ImageGame } from "./ImageGame";
 import { QuizGame } from "./QuizGame";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <HashRouter>
-        <Routes>
-          <Route path="/quiz" element={<QuizGame />} />
-          <Route path="/" element={<ImageGame />} />
-        </Routes>
-      </HashRouter>
-    </BrowserRouter>
+    <HashRouter>
+      <Routes>
+        <Route path="/quiz" element={<QuizGame />} />
+        <Route path="/" element={<ImageGame />} />
+      </Routes>
+    </HashRouter>
   );
 }
